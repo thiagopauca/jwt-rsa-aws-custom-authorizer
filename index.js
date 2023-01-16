409a19a78d7e91e60ab10c851d27edca07d53c3b
Thiago Valverde de Souza <thiago@pauca.co>
2023-01-16 12:23:58 -0300

    Adding claims to request headers

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ let data;
 // Lambda function index.handler - thin wrapper around lib.authenticate
 module.exports.handler = async (event, context, callback) => {
   try {
+    console.log("Event: ", event)
     data = await lib.authenticate(event);
+    console.log("Data: ", data)
   }
   catch (err) {
       console.log(err);
